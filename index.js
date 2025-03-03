cddfda772ba314a8bba45275cec865cbda240ad0
Brian <musyimib449@gmail.com>
2025-03-03 14:49:08 +0300

    event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+  
+    if (input) { 
+      input.addEventListener('click', function() {
+        alert('I was clicked!');
+      });
+    }
 }
